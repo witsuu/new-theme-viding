47da809c523e31797745bad7bcdae9dff0c2fcc2
witsuu <witsudi217@gmail.com>
2022-03-21 06:02:46 +0700

    add image brush sm

---
+++ chinese/assets/js/app.js
@@ -0,0 +1,9 @@
+// const cd = document.querySelector('.countdown').getAttribute('date')
+
+// Countdown(cd)
+
+
+
+$(document).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > $('nav').height())
+});
