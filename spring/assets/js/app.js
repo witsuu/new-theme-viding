f9b0bee7e9823dae27d546219c24fd1cecb806b5
witsuu <witsudi217@gmail.com>
2022-03-20 18:07:38 +0700

    added theme spring

---
+++ spring/assets/js/app.js
@@ -0,0 +1,7 @@
+const cd = document.querySelector('.countdown').getAttribute('date')
+
+Countdown(cd)
+
+$(document).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > $('nav').height())
+});
