f86b598533a39b317fb3b80b962f1e7d5b11f512
witsuu <witsudi217@gmail.com>
2022-03-22 21:53:28 +0700

    add revision tema spring

--- chinese/assets/js/app.js
+++ chinese/assets/js/app.js
@@ -7,3 +7,16 @@ Countdown(cd)
 $(document).scroll(function () {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > $('nav').height())
 });
+
+$(".zoom-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    gallery: {
+        enabled: true,
+    },
+    zoom: {
+        enabled: true,
+        easing: "ease-in-out",
+    },
+});
