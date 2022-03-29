d4a7da5f681457383f244d12121c727a03c44e3e
witsuu <witsudi217@gmail.com>
2022-03-29 13:11:33 +0700

    update theme chinnese

--- chinese/assets/js/app.js
+++ chinese/assets/js/app.js
@@ -34,8 +34,6 @@ const cloudRight1 = $('.cloud-right-1')
 const cloudRight2 = $('.cloud-right-2')
 
 const cloudLeft3 = $('.cloud-left-3')
-const cloudLeft4 = $(".cloud-left-4")
-const cloudRight3 = $('.cloud-right-3')
 const cloudRight4 = $('.cloud-right-4')
 
 gsap.to([cloudLeft1, cloudLeft3], {
@@ -43,10 +41,10 @@ gsap.to([cloudLeft1, cloudLeft3], {
     repeat: -1,
     yoyo: true,
     ease: Linear.easeNone,
-    duration: 3
+    duration: 5
 })
 
-gsap.to([cloudLeft2, cloudLeft4], {
+gsap.to([cloudLeft2], {
     x: 15,
     repeat: -1,
     yoyo: true,
@@ -54,12 +52,12 @@ gsap.to([cloudLeft2, cloudLeft4], {
     duration: 5
 })
 
-gsap.to([cloudRight1, cloudRight3], {
+gsap.to([cloudRight1], {
     x: -20,
     repeat: -1,
     yoyo: true,
     ease: Linear.easeNone,
-    duration: 3.5
+    duration: 5
 })
 gsap.to([cloudRight2, cloudRight4], {
     x: -15,
