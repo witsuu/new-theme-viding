4cb72a8689a5b1933d7bc49f2db1fa9e8f5b28b9
witsuu <witsudi217@gmail.com>
2022-04-01 22:06:59 +0700

    add theme winter and revision all theme

---
+++ winter/assets/js/app.js
@@ -0,0 +1,22 @@
+// const cd = document.querySelector('.countdown').getAttribute('date')
+
+// Countdown(cd)
+
+
+
+$(document).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > $('nav').height())
+});
+
+$(".zoom-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    gallery: {
+        enabled: true,
+    },
+    zoom: {
+        enabled: true,
+        easing: "ease-in-out",
+    },
+});
