de5a5b4db2858dc62969165a2233d703ba379dc2
witsuu <witsudi217@gmail.com>
2022-05-14 20:05:52 +0700

    add theme Ramadhan

---
+++ ramadhan/assets/js/app.js
@@ -0,0 +1,28 @@
+$("body").css('overflow-y', 'hidden')
+
+$("#btn-envelope").on("click", function () {
+    $("body").css("overflow-y", "auto");
+
+    $(".envelope-wrap").css("transform", "translateY(-100%)");
+});
+
+$(document).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > $('nav').height())
+});
+
+const cd = document.querySelector('.countdown').getAttribute('date')
+
+Countdown(cd)
+
+$("#zoom-gallery").magnificPopup({
+    delegate: "li a",
+    type: "image",
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    gallery: {
+        enabled: true,
+    },
+    zoom: {
+        enabled: true,
+        easing: "ease-in-out",
+    },
+});
