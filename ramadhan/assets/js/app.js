fbce4f120eddcfb23c655e0df2f987a05d76a914
witsuu <witsudi217@gmail.com>
2022-05-15 09:28:51 +0700

    revision theme ramadhan

--- ramadhan/assets/js/app.js
+++ ramadhan/assets/js/app.js
@@ -3,7 +3,7 @@ $("body").css('overflow-y', 'hidden')
 $("#btn-envelope").on("click", function () {
     $("body").css("overflow-y", "auto");
 
-    $(".envelope-wrap").css("transform", "translateY(-100%)");
+    $(".envelope-wrap").addClass("open");
 });
 
 $(document).scroll(function () {
