8e2975531d7329db41d1105a849faea7858d39d6
witsuu <witsudi217@gmail.com>
2022-05-21 19:37:39 +0700

    handle theme tuntrum

---
+++ fantasy-cloud/assets/js/app.js
@@ -0,0 +1,124 @@
+"use strict";
+
+$(document).ready(() => {
+  $('body').css('overflowY', 'hidden');
+  $('.main-app').css("visibility", 'hidden');
+
+  $("#btn-envelope").on('click', function () {
+    $("#envelope-wrap").css('transform', 'translateY(-100%)')
+    $("#envelope-wrap").css('opacity', 0)
+
+    $('body').css('overflowY', 'auto');
+    $('.main-app').css('visibility', 'visible');
+  })
+
+  $(".zoom-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    gallery: {
+      enabled: true,
+    },
+    zoom: {
+      enabled: true,
+      easing: "ease-in-out",
+    },
+  });
+
+  var slickopts = {
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    rows: 2, // Removes the linear order. Would expect card 5 to be on next row, not stacked in groups.
+    responsive: [
+      { breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      { breakpoint: 776,
+        settings: {
+          slidesToShow: 1,
+          rows: 1 // This doesn't appear to work in responsive (Mac/Chrome)
+        }
+      }]
+  };
+
+  $('.carousel').slick(slickopts);
+});
+
+// countdown
+
+(function ($) {
+  "use strict";
+  $.fn.aksCountDown = function (options) {
+    const aks = $(this);
+    var settings = $.extend(
+      {
+        endTime: "",
+        refresh: 1000,
+        onEnd: function () {}
+      },
+      options
+    );
+    return this.each(function (i) {
+      function endTimeCheck(d1, d2) {
+        return (
+          d1.getFullYear() === d2.getFullYear() &&
+          d1.getMonth() === d2.getMonth() &&
+          d1.getDate() === d2.getDate()
+        );
+      }
+      function countTimer() {
+        var endTime = new Date(settings.endTime);
+        endTime = Date.parse(endTime) / 1000;
+
+        var now = new Date();
+        now = Date.parse(now) / 1000;
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - days * 86400) / 3600);
+        var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
+        var seconds = Math.floor(
+          timeLeft - days * 86400 - hours * 3600 - minutes * 60
+        );
+
+        if (hours < "10") {
+          hours = "0" + hours;
+        }
+        if (minutes < "10") {
+          minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+          seconds = "0" + seconds;
+        }
+
+        $(aks).find("[data-days]").html(days);
+        $(aks).find("[data-hours]").html(hours);
+        $(aks).find("[data-minutes]").html(minutes);
+        $(aks).find("[data-seconds]").html(seconds);
+      }
+      var now = new Date();
+      var endTime = new Date(settings.endTime);
+
+      if (endTimeCheck(now, endTime) === true) {
+        settings.onEnd.call(aks);
+      } else {
+        setInterval(function () {
+          countTimer();
+        }, settings.refresh);
+      }
+    });
+  };
+})(jQuery);
+
+$("#timer").aksCountDown({
+  endTime: "15 June 2022 9:56:00 GMT+01:00",
+  onEnd: function () {
+    $(this).html('<div class="timer-end">Finished Time</div>');
+  }
+});
